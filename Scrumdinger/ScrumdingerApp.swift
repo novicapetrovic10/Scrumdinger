84bd2c337fc8df621c43976135afdf80ffed451e
Novica Petrovic <novica.petrovic@clearscore.com>
2020-12-26 22:23:48 +0000

    Initial Commit

---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Novica Petrovic on 26/12/2020.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
